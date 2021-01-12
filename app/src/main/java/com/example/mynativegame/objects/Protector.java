1c3d061c9ccbad3a6ae124719003aa29f3045264
LifeisRunway <39906544+LifeisRunway@users.noreply.github.com>
2021-01-12 09:26:47 +0300

    Create Protector.java

---
+++ app/src/main/java/com/example/mynativegame/objects/Protector.java
@@ -0,0 +1,48 @@
+package com.example.mynativegame.objects;
+
+import android.graphics.Rect;
+
+import com.example.my_game_module.AnimationGameFW;
+import com.example.my_game_module.GraphicsFW;
+import com.example.my_game_module.ObjectFW;
+import com.example.my_game_module.utils.UtilRandomFW;
+import com.example.my_game_module.utils.UtilResource;
+
+public class Protector extends ObjectFW {
+    
+    AnimationGameFW animProtector;
+
+    public Protector(int maxScreenX, int maxScreenY, int minScreenY) {
+        this.maxScreenX = maxScreenX;
+        this.maxScreenY = maxScreenY - UtilResource.spriteProtector.get(0).getHeight();
+        this.minScreenY = minScreenY;
+        this.minScreenX = 0;
+        x = maxScreenX;
+        y = UtilRandomFW.getGap(minScreenY, maxScreenY);
+        radius = UtilResource.spriteProtector.get(0).getWidth() / 4;
+        hitBox = new Rect(x, y,
+                UtilResource.spriteProtector.get(0).getWidth(),
+                UtilResource.spriteProtector.get(0).getHeight());
+    }
+
+    public void update(float speedPlayer) {
+        x -= speed;
+        x -= speedPlayer;
+               
+        if(x < minScreenX) {
+            y = UtilRandomFW.getGap(minScreenY, maxScreenY);
+        }
+        
+        animProtector.runAnimation();
+        
+        hitBox = new Rect(x, y,
+                UtilResource.spriteEnemy.get(0).getWidth(),
+                UtilResource.spriteEnemy.get(0).getHeight());
+    }
+
+    public void drawing (GraphicsFW graphicsFW) {
+        animProtector.drawingAnimation(graphicsFW, x, y);
+    }
+
+  
+}
