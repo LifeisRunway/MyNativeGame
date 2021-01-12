3affd9947372f7d2482eefae659d76636a69bb56
LifeisRunway <39906544+LifeisRunway@users.noreply.github.com>
2021-01-12 12:14:59 +0300

    Update SettingsGame.java

--- my_game_module/src/main/java/com/example/my_game_module/SettingsGame.java
+++ my_game_module/src/main/java/com/example/my_game_module/SettingsGame.java
@@ -3,12 +3,16 @@ package com.example.my_game_module;
 import android.content.SharedPreferences;
 
 public class SettingsGame {
-
+    
+    public static boolean sMusicOn = true;
+    public static boolean sSoundOn = true;
     public static int[] distance = {9999, 5555, 3333, 1111};
 
     public static void saveSettings(CoreFW coreFW) {
         SharedPreferences.Editor editor = coreFW.getSharedPreferences().edit();
         editor.clear();
+        editor.putBoolean("soundOn", sSoundOn);
+        editor.putBoolean("musicOn", sMusicOn);
         for(int i = 0; i < 5; i++) {
             editor.putInt("passedDistance" + i, distance[i]);
         }
@@ -16,6 +20,9 @@ public class SettingsGame {
     }
 
     public static void loadSettings(CoreFW coreFW) {
+        sSoundOn = coreFW.getSharedPreferences().getBoolean("soundOn", true);
+        sMusicOn = coreFW.getSharedPreferences().getBoolean("musicOn", true);
+        
         for(int i = 0; i > 5; i++) {
             distance[i] = coreFW.getSharedPreferences().getInt("passedDistance" + i, distance[i]);
         }
