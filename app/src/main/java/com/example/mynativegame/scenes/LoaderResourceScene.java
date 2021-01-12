8238d7ec695a40671bd7683b0b1b066b435090f2
LifeisRunway <39906544+LifeisRunway@users.noreply.github.com>
2021-01-12 11:04:43 +0300

    Create LoaderResourceScene.java

---
+++ app/src/main/java/com/example/mynativegame/scenes/LoaderResourceScene.java
@@ -0,0 +1,36 @@
+package com.example.mynativegame.scenes
+
+public class LoaderResourceScene extends SceneGame {
+  
+    
+  
+    public LoaderResourceScene(CoreFW coreFW) {
+        super(coreFW);
+    }
+    
+    @Override
+    public void update() {
+        
+    }
+
+    @Override
+    public void drawing() {
+        
+    }
+  
+    @Override
+    public void pause() {
+       
+    }
+
+    @Override
+    public void resume() {
+        
+    }
+
+    @Override
+    public void dispose() {
+      
+    }
+  
+}
