b6478453b99fdc30c7fd2818d9fd4548d4fde508
LifeisRunway <39906544+LifeisRunway@users.noreply.github.com>
2021-01-12 11:17:38 +0300

    Update LoaderResourceScene.java

--- app/src/main/java/com/example/mynativegame/scenes/LoaderResourceScene.java
+++ app/src/main/java/com/example/mynativegame/scenes/LoaderResourceScene.java
@@ -1,16 +1,16 @@
 package com.example.mynativegame.scenes
 
 public class LoaderResourceScene extends SceneGame {
-  
-    
-  
+        
     public LoaderResourceScene(CoreFW coreFW) {
         super(coreFW);
+        LoaderTask loaderTask = new LoaderTask(coreFW);
+        loaderTask.execute();
     }
     
     @Override
     public void update() {
-        
+         coreFW.setScene(new MainMenuScene(coreFW));
     }
 
     @Override
