015f453b70fe58b8f0ab8cbaa481ca4cfadb53d9
LifeisRunway <39906544+LifeisRunway@users.noreply.github.com>
2021-01-12 10:25:52 +0300

    Update GameManager.java

--- app/src/main/java/com/example/mynativegame/classes/GameManager.java
+++ app/src/main/java/com/example/mynativegame/classes/GameManager.java
@@ -11,16 +11,17 @@ import com.example.mynativegame.objects.MainPlayer;
 
 public class GameManager  {
 
-    private int maxScreenX;
-    private int maxScreenY;
-    private int minScreenX;
-    private int minScreenY;
-
-    private int passedDistance;
-    private int currentSpeedPlayer;
-    private int currentShieldsPlayer;
-
     public final static double SPEED_ANIMATION = 3;
+    
+    private int mMaxScreenX;
+    private int mMaxScreenY;
+    private int mMinScreenY;
+
+    private int mPassedDistance;
+    private int mCurrentSpeedPlayer;
+    private int mCurrentShieldsPlayer;
+
+    
     public static boolean gameOver;
 
     MainPlayer mainPlayer;
@@ -31,11 +32,11 @@ public class GameManager  {
 
     public GameManager(CoreFW coreFW, int sceneWidth, int sceneHeight) {
         hud = new HUD(coreFW);
-        this.maxScreenX = sceneWidth;
-        this.maxScreenY = sceneHeight;
+        mMaxScreenX = sceneWidth;
+        mMaxScreenY = sceneHeight;
+        mMinScreenY = hud.getHEIGHT_HUD();
         gameOver = false;
-        minScreenY = hud.getHEIGHT_HUD();
-        minScreenX = 0;
+        
         mainPlayer = new MainPlayer(coreFW, maxScreenX, maxScreenY, minScreenY);
         generatorBackground = new GeneratorBackground(sceneWidth, sceneHeight, minScreenY);
         generatorEnemy = new GeneratorEnemy(sceneWidth,sceneHeight,minScreenY);
@@ -48,11 +49,11 @@ public class GameManager  {
         generatorEnemy.update(mainPlayer.getSpeed());
         generatorGifts.update(mainPlayer.getSpeed());
         
-        passedDistance += mainPlayer.getSpeed();
-        currentSpeedPlayer = (int) mainPlayer.getSpeed() * 60;
-        currentShieldsPlayer = mainPlayer.getShieldsPlayer();
+        mPassedDistance += mainPlayer.getSpeed();
+        mCurrentSpeedPlayer = (int) mainPlayer.getSpeed() * 60;
+        mCurrentShieldsPlayer = mainPlayer.getShieldsPlayer();
 
-        hud.update(passedDistance,currentSpeedPlayer,currentShieldsPlayer);
+        hud.update(mPassedDistance, mCurrentSpeedPlayer, mCurrentShieldsPlayer);
 
         checkHit();
 
@@ -85,6 +86,6 @@ public class GameManager  {
     }
 
     public int getPassedDistance() {
-        return passedDistance;
+        return mPassedDistance;
     }
 }
